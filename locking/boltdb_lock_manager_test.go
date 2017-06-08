c68adac63f7096fe13be5536ed39cea3868baf32
Luke Kysow <luke.kysow@hootsuite.com>
2017-06-07 23:50:53 -0700

    Switch to regular go testing style

--- locking/boltdb_lock_manager_test.go
+++ locking/boltdb_lock_manager_test.go
@@ -1,238 +1,297 @@
 package locking_test
 
 import (
-	. "github.com/hootsuite/atlantis/locking"
+	"github.com/hootsuite/atlantis/locking"
+	. "github.com/hootsuite/atlantis/testing_util"
 
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
 	"github.com/boltdb/bolt"
 	"os"
+	"testing"
+	"io/ioutil"
+	"github.com/pkg/errors"
+
 	"time"
 )
 
-var _ = Describe("BoltDBLockManager", func() {
-	var db *bolt.DB
-	var locker *BoltDBLockManager
-	var run Run
-	lockBucket := "locks"
-	BeforeEach(func() {
-		db = NewTestDB()
-		locker = NewBoltDBLockManager(db, lockBucket)
-		run = Run{
-			RepoFullName: "owner/repo",
-			Path: "parent/child",
-			Env: "default",
-			PullNum: 1,
-			User: "user",
-			Timestamp: time.Now(),
-		}
-	})
-	AfterEach(func() {
-		os.Remove(db.Path())
-		db.Close()
-	})
-	var addLock = func(locker *BoltDBLockManager, run Run) {
-		res, err := locker.TryLock(run)
-		Expect(err).ToNot(HaveOccurred())
-		Expect(res.LockAcquired).To(BeTrue())
-		Expect(res.LockingRun).To(Equal(run))
+var lockBucket = "bucket"
+var run = locking.Run{
+	RepoFullName: "owner/repo",
+	Path: "parent/child",
+	Env: "default",
+	PullNum: 1,
+	User: "user",
+	Timestamp: time.Now(),
+}
+
+func TestListNoLocks(t *testing.T) {
+	t.Log("listing locks when there are none should return an empty list")
+	db, b := newTestDB()
+	defer cleanupDB(db)
+	ls, err := b.ListLocks()
+	Ok(t, err)
+	Equals(t, 0, len(ls))
+}
+
+func TestListOneLock(t *testing.T) {
+	t.Log("listing locks when there is one should return it")
+	db, b := newTestDB()
+	defer cleanupDB(db)
+	_, err := b.TryLock(run)
+	Ok(t, err)
+	ls, err := b.ListLocks()
+	Ok(t, err)
+	Equals(t, 1, len(ls))
+	Equals(t, run, ls[run.StateKey()])
+}
+
+func TestListMultipleLocks(t *testing.T) {
+	t.Log("listing locks when there are multiple should return them")
+	db, b := newTestDB()
+	defer cleanupDB(db)
+
+	// add multiple locks
+	_, err := b.TryLock(run)
+	Ok(t, err)
+
+	run2 := run
+	run2.RepoFullName = "different"
+	_, err = b.TryLock(run2)
+	Ok(t, err)
+
+	run3 := run
+	run3.RepoFullName = "different again"
+	_, err = b.TryLock(run3)
+	Ok(t, err)
+
+	run4 := run
+	run4.RepoFullName = "different again again"
+	_, err = b.TryLock(run4)
+	Ok(t, err)
+
+	ls, err := b.ListLocks()
+	Ok(t, err)
+	Equals(t, 4, len(ls))
+	Equals(t, run, ls[run.StateKey()])
+	Equals(t, run2, ls[run2.StateKey()])
+	Equals(t, run3, ls[run3.StateKey()])
+	Equals(t, run4, ls[run4.StateKey()])
+}
+
+func TestListAddRemove(t *testing.T) {
+	t.Log("listing after adding and removing should return none")
+	db, b := newTestDB()
+	defer cleanupDB(db)
+	_, err := b.TryLock(run)
+	Ok(t, err)
+	b.Unlock(run.StateKey())
+
+	ls, err := b.ListLocks()
+	Ok(t, err)
+	Equals(t, 0, len(ls))
+}
+
+func TestLockingNoLocks(t *testing.T) {
+	t.Log("with no locks yet, lock should succeed")
+	db, b := newTestDB()
+	defer cleanupDB(db)
+	r, err := b.TryLock(run)
+	Ok(t, err)
+	Equals(t, true, r.LockAcquired)
+	Equals(t, run, r.LockingRun)
+	Equals(t, run.StateKey(), r.LockID)
+}
+
+func TestLockingExistingLock(t *testing.T) {
+	t.Log("if there is an existing lock, lock should...")
+	db, b := newTestDB()
+	defer cleanupDB(db)
+	_, err := b.TryLock(run)
+	Ok(t, err)
+
+	t.Log("...succeed if the new run has a different path")
+	{
+		new := run
+		new.Path = "different/path"
+		r, err := b.TryLock(new)
+		Ok(t, err)
+		Equals(t, true, r.LockAcquired)
+		Equals(t, new, r.LockingRun)
+		Equals(t, new.StateKey(), r.LockID)
 	}
 
-	Describe("listing locks", func() {
-		Context("with no locks", func() {
-			It("should return 0", func() {
-				Expect(locker.ListLocks()).To(BeEmpty())
-			})
-		})
-		Context("with one lock", func() {
-			BeforeEach(func() {
-				addLock(locker, run)
-			})
-			It("should return it", func() {
-				Expect(locker.ListLocks()).To(HaveLen(1))
-				Expect(locker.ListLocks()).To(Equal(map[string]Run{
-					run.StateKey(): run,
-				}))
-			})
-		})
-		Context("with two locks", func() {
-			var run2 Run
-			BeforeEach(func() {
-				addLock(locker, run)
-				run2 = run
-				run2.RepoFullName = "new/repo"
-				addLock(locker, run2)
-			})
-			It("should return them", func() {
-				Expect(locker.ListLocks()).To(HaveLen(2))
-				Expect(locker.ListLocks()).To(HaveKeyWithValue(run.StateKey(), run))
-				Expect(locker.ListLocks()).To(HaveKeyWithValue(run2.StateKey(), run2))
-			})
-		})
-		Context("with one lock added and then unlocked", func() {
-			BeforeEach(func() {
-				addLock(locker, run)
-				err := locker.Unlock(run.StateKey())
-				Expect(err).NotTo(HaveOccurred())
-			})
-			It("should return 0", func() {
-				Expect(locker.ListLocks()).To(BeEmpty())
-			})
-		})
-	})
+	t.Log("...succeed if the new run has a different environment")
+	{
+		new := run
+		new.Env = "different-env"
+		r, err := b.TryLock(new)
+		Ok(t, err)
+		Equals(t, true, r.LockAcquired)
+		Equals(t, new, r.LockingRun)
+		Equals(t, new.StateKey(), r.LockID)
+	}
 
-	Describe("locking", func() {
-		Context("with no current locks", func() {
-			It("should succeed", func() {
-				Expect(locker.TryLock(run)).To(Equal(TryLockResponse{
-					LockAcquired: true,
-					LockingRun: run,
-					LockID: run.StateKey(),
-				}))
-			})
-		})
-		Context("with an existing lock", func() {
-			BeforeEach(func() {
-				addLock(locker, run)
-			})
-			It("should succeed if the new run has a different path", func() {
-				newRun := run
-				newRun.Path = "different/path"
-				Expect(locker.TryLock(newRun)).To(Equal(TryLockResponse{
-					LockAcquired: true,
-					LockingRun: newRun,
-					LockID: newRun.StateKey(),
-				}))
-			})
-			It("should succeed if the new run has a different environment", func() {
-				newRun := run
-				newRun.Env = "different-env"
-				Expect(locker.TryLock(newRun)).To(Equal(TryLockResponse{
-					LockAcquired: true,
-					LockingRun: newRun,
-					LockID: newRun.StateKey(),
-				}))
-			})
-			It("should succeed if the new run has a different repoName", func() {
-				newRun := run
-				newRun.RepoFullName = "new/repo"
-				Expect(locker.TryLock(newRun)).To(Equal(TryLockResponse{
-					LockAcquired: true,
-					LockingRun: newRun,
-					LockID: newRun.StateKey(),
-				}))
-			})
-			It("should not succeed if the new run only has a different pullNum and return the locking run", func() {
-				newRun := run
-				newRun.PullNum = 2
-				Expect(locker.TryLock(newRun)).To(Equal(TryLockResponse{
-					LockAcquired: false,
-					LockingRun: run,
-					LockID: run.StateKey(),
-				}))
-			})
-		})
-	})
+	t.Log("...succeed if the new run has a different repoName")
+	{
+		new := run
+		new.RepoFullName = "new/repo"
+		r, err := b.TryLock(new)
+		Ok(t, err)
+		Equals(t, true, r.LockAcquired)
+		Equals(t, new, r.LockingRun)
+		Equals(t, new.StateKey(), r.LockID)
+	}
 
-	Describe("unlocking", func() {
-		Context("with no locks", func() {
-			It("should unlock a key that doesn't exist", func() {
-				Expect(locker.Unlock("any-lock-id")).To(Succeed())
-			})
-			It("should unlock a key that doesn't exist even if it's an empty string", func() {
-				Expect(locker.Unlock("")).To(Succeed())
-			})
-		})
-		Context("with an existing lock", func() {
-			BeforeEach(func() {
-				addLock(locker, run)
-			})
-			It("should unlock a key that doesn't exist", func() {
-				Expect(locker.Unlock("any-lock-id")).To(Succeed())
-			})
-			It("should unlock the run successfully", func() {
-				Expect(locker.Unlock(run.StateKey())).To(Succeed())
-				Expect(locker.ListLocks()).To(BeEmpty())
-			})
-		})
-	})
+	t.Log("...not succeed if the new run only has a different pullNum")
+	{
+		new := run
+		new.PullNum = run.PullNum + 1
+		r, err := b.TryLock(new)
+		Ok(t, err)
+		Equals(t, false, r.LockAcquired)
+		Equals(t, run, r.LockingRun)
+		Equals(t, run.StateKey(), r.LockID)
+	}
+}
 
-	Describe("FindLocksForPull", func() {
-		Context("with no locks", func() {
-			It("should return no locks", func() {
-				Expect(locker.FindLocksForPull("owner/repo", 1)).To(BeEmpty())
-			})
-		})
-		Context("with one lock", func() {
-			BeforeEach(func() {
-				addLock(locker, run)
-			})
-			It("should return no locks from the same repo but different pull num", func() {
-				Expect(locker.FindLocksForPull(run.RepoFullName, run.PullNum + 1)).To(BeEmpty())
-			})
-			It("should return no locks from a different repo but the same pull num", func() {
-				Expect(locker.FindLocksForPull(run.RepoFullName + "dif", run.PullNum)).To(BeEmpty())
-			})
-			It("should return the one lock when called with that repo and pull num", func () {
-				Expect(locker.FindLocksForPull(run.RepoFullName, run.PullNum)).To(Equal([]string{run.StateKey()}))
-			})
-		})
-		Context("with one lock added and then unlocked", func() {
-			BeforeEach(func() {
-				addLock(locker, run)
-				err := locker.Unlock(run.StateKey())
-				Expect(err).NotTo(HaveOccurred())
-			})
-			It("should return 0 locks from that repo and pull num", func () {
-				Expect(locker.FindLocksForPull(run.RepoFullName, run.PullNum)).To(BeEmpty())
-			})
-		})
-		Context("with two locks from the different repos but same pull nums", func() {
-			var run2 Run
-			BeforeEach(func() {
-				addLock(locker, run)
-				run2 = run
-				run2.RepoFullName = run.RepoFullName + "dif"
-				addLock(locker, run2)
-			})
-			It("should return the lock from the matching repo and pull num and nothing else", func() {
-				Expect(locker.FindLocksForPull(run.RepoFullName, run.PullNum)).To(Equal([]string{run.StateKey()}))
-				Expect(locker.FindLocksForPull(run2.RepoFullName, run2.PullNum)).To(Equal([]string{run2.StateKey()}))
-			})
-		})
-		Context("with two locks from the same repos and pull num but with different paths", func() {
-			var run2 Run
-			BeforeEach(func() {
-				addLock(locker, run)
-				run2 = run
-				run2.Path = run.Path + "/child"
-				addLock(locker, run2)
-			})
-			It("should return both runs", func() {
-				Expect(locker.FindLocksForPull(run.RepoFullName, run.PullNum)).To(HaveLen(2))
-				Expect(locker.FindLocksForPull(run.RepoFullName, run.PullNum)).To(ContainElement(run.StateKey()))
-				Expect(locker.FindLocksForPull(run.RepoFullName, run.PullNum)).To(ContainElement(run2.StateKey()))
-			})
-		})
-		Context("with two locks from the same repos and pull num but with different envs", func() {
-			var run2 Run
-			BeforeEach(func() {
-				addLock(locker, run)
-				run2 = run
-				run2.Env = run.Env + "dif"
-				addLock(locker, run2)
-			})
-			It("should return both runs", func() {
-				Expect(locker.FindLocksForPull(run.RepoFullName, run.PullNum)).To(HaveLen(2))
-				Expect(locker.FindLocksForPull(run.RepoFullName, run.PullNum)).To(ContainElement(run.StateKey()))
-				Expect(locker.FindLocksForPull(run.RepoFullName, run.PullNum)).To(ContainElement(run2.StateKey()))
-			})
-		})
-	})
-})
+func TestUnlockingNoLocks(t *testing.T) {
+	t.Log("unlocking with no locks should succeed")
+	db, b := newTestDB()
+	defer cleanupDB(db)
 
-// NewTestDB returns a TestDB using a temporary path.
-func NewTestDB() (*bolt.DB, *locking.BoltDBLockManager) {
+	Ok(t, b.Unlock("any-lock-id"))
+}
+
+func TestUnlocking(t *testing.T) {
+	t.Log("unlocking with an existing lock should succeed")
+	db, b := newTestDB()
+	defer cleanupDB(db)
+
+	b.TryLock(run)
+	Ok(t, b.Unlock(run.StateKey()))
+
+	// should be no locks listed
+	ls, err := b.ListLocks()
+	Ok(t, err)
+	Equals(t, 0, len(ls))
+
+	// should be able to re-lock that repo with a new pull num
+	new := run
+	new.PullNum = run.PullNum + 1
+	r, err := b.TryLock(new)
+	Ok(t, err)
+	Equals(t, true, r.LockAcquired)
+}
+
+func TestUnlockingMultiple(t *testing.T) {
+	t.Log("unlocking and locking multiple locks should succeed")
+	db, b := newTestDB()
+	defer cleanupDB(db)
+
+	b.TryLock(run)
+
+	new := run
+	new.RepoFullName = "new/repo"
+	b.TryLock(new)
+
+	new2 := run
+	new2.Path = "new/path"
+	b.TryLock(new2)
+
+	new3 := run
+	new3.Env = "new/env"
+	b.TryLock(new3)
+
+	// now try and unlock them
+	Ok(t, b.Unlock(new3.StateKey()))
+	Ok(t, b.Unlock(new2.StateKey()))
+	Ok(t, b.Unlock(new.StateKey()))
+	Ok(t, b.Unlock(run.StateKey()))
+
+	// should be none left
+	ls, err := b.ListLocks()
+	Ok(t, err)
+	Equals(t, 0, len(ls))
+}
+
+func TestFindLocksNone(t *testing.T) {
+	t.Log("find should return no locks when there are none")
+	db, b := newTestDB()
+	defer cleanupDB(db)
+
+	ls, err := b.FindLocksForPull("any/repo", 1)
+	Ok(t, err)
+	Equals(t, 0, len(ls))
+}
+
+func TestFindLocksOne(t *testing.T) {
+	t.Log("with one lock find should...")
+	db, b := newTestDB()
+	defer cleanupDB(db)
+	_, err := b.TryLock(run)
+	Ok(t, err)
+
+	t.Log("...return no locks from the same repo but different pull num")
+	{
+		ls, err := b.FindLocksForPull(run.RepoFullName, run.PullNum + 1)
+		Ok(t, err)
+		Equals(t, 0, len(ls))
+	}
+	t.Log("...return no locks from a different repo but the same pull num")
+	{
+		ls, err := b.FindLocksForPull(run.RepoFullName + "dif", run.PullNum)
+		Ok(t, err)
+		Equals(t, 0, len(ls))
+	}
+	t.Log("...return the one lock when called with that repo and pull num")
+	{
+		ls, err := b.FindLocksForPull(run.RepoFullName, run.PullNum)
+		Ok(t, err)
+		Equals(t, 1, len(ls))
+		Equals(t, run.StateKey(), ls[0])
+	}
+}
+
+func TestFindLocksAfterUnlock(t *testing.T) {
+	t.Log("after locking and unlocking find should return no locks")
+	db, b := newTestDB()
+	defer cleanupDB(db)
+	_, err := b.TryLock(run)
+	Ok(t, err)
+	Ok(t, b.Unlock(run.StateKey()))
+
+	ls, err := b.FindLocksForPull(run.RepoFullName, run.PullNum)
+	Ok(t, err)
+	Equals(t, 0, len(ls))
+}
+
+func TestFindMultipleMatching(t *testing.T) {
+	t.Log("find should return all matching lock ids")
+	db, b := newTestDB()
+	defer cleanupDB(db)
+	_, err := b.TryLock(run)
+	Ok(t, err)
+
+	// add additional locks with the same repo and pull num but different paths/envs
+	new := run
+	new.Path = "dif/path"
+	_, err = b.TryLock(new)
+	Ok(t, err)
+	new2 := run
+	new2.Env = "new-env"
+	_, err = b.TryLock(new2)
+	Ok(t, err)
+
+	// should get all of them back
+	ls, err := b.FindLocksForPull(run.RepoFullName, run.PullNum)
+	Ok(t, err)
+	Equals(t, 3, len(ls))
+	ContainsStr(t, run.StateKey(), ls)
+	ContainsStr(t, new.StateKey(), ls)
+	ContainsStr(t, new2.StateKey(), ls)
+}
+
+// newTestDB returns a TestDB using a temporary path.
+func newTestDB() (*bolt.DB, *locking.BoltDBLockManager) {
 	// Retrieve a temporary path.
 	f, err := ioutil.TempFile("", "")
 	if err != nil {
@@ -247,13 +306,19 @@ func NewTestDB() (*bolt.DB, *locking.BoltDBLockManager) {
 		panic(errors.Wrap(err, "could not start bolt DB"))
 	}
 	if err := db.Update(func(tx *bolt.Tx) error {
-		if _, err := tx.CreateBucketIfNotExists([]byte(LockBucket)); err != nil {
+		if _, err := tx.CreateBucketIfNotExists([]byte(lockBucket)); err != nil {
 			return errors.Wrap(err, "failed to create bucket")
 		}
 		return nil
 	}); err != nil {
 		panic(errors.Wrap(err, "could not create bucket"))
 	}
-	b, _ := locking.NewBoltDBLockManagerWithDB(db, LockBucket)
+	b, _ := locking.NewBoltDBLockManagerWithDB(db, lockBucket)
 	return db, b
 }
+
+func cleanupDB(db *bolt.DB) {
+	os.Remove(db.Path())
+	db.Close()
+}
+
