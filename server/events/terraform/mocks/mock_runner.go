f1e45068c74ef0caf1635e6ab4603b06fdd86320
Anubhav Mishra <anubhavmishra@me.com>
2017-10-28 19:53:11 -0700

    renaming RunInitAndEnv() to RunInitAndWorkspace()

--- server/events/terraform/mocks/mock_runner.go
+++ server/events/terraform/mocks/mock_runner.go
@@ -4,10 +4,11 @@
 package mocks
 
 import (
+	"reflect"
+
 	go_version "github.com/hashicorp/go-version"
 	logging "github.com/hootsuite/atlantis/server/logging"
 	pegomock "github.com/petergtz/pegomock"
-	"reflect"
 )
 
 type MockRunner struct {
@@ -46,9 +47,9 @@ func (mock *MockRunner) RunCommandWithVersion(log *logging.SimpleLogger, path st
 	return ret0, ret1
 }
 
-func (mock *MockRunner) RunInitAndEnv(log *logging.SimpleLogger, path string, env string, extraInitArgs []string, version *go_version.Version) ([]string, error) {
+func (mock *MockRunner) RunInitAndWorkspace(log *logging.SimpleLogger, path string, env string, extraInitArgs []string, version *go_version.Version) ([]string, error) {
 	params := []pegomock.Param{log, path, env, extraInitArgs, version}
-	result := pegomock.GetGenericMockFrom(mock).Invoke("RunInitAndEnv", params, []reflect.Type{reflect.TypeOf((*[]string)(nil)).Elem(), reflect.TypeOf((*error)(nil)).Elem()})
+	result := pegomock.GetGenericMockFrom(mock).Invoke("RunInitAndWorkspace", params, []reflect.Type{reflect.TypeOf((*[]string)(nil)).Elem(), reflect.TypeOf((*error)(nil)).Elem()})
 	var ret0 []string
 	var ret1 error
 	if len(result) != 0 {
@@ -140,23 +141,23 @@ func (c *Runner_RunCommandWithVersion_OngoingVerification) GetAllCapturedArgumen
 	return
 }
 
-func (verifier *VerifierRunner) RunInitAndEnv(log *logging.SimpleLogger, path string, env string, extraInitArgs []string, version *go_version.Version) *Runner_RunInitAndEnv_OngoingVerification {
+func (verifier *VerifierRunner) RunInitAndWorkspace(log *logging.SimpleLogger, path string, env string, extraInitArgs []string, version *go_version.Version) *Runner_RunInitAndWorkspace_OngoingVerification {
 	params := []pegomock.Param{log, path, env, extraInitArgs, version}
-	methodInvocations := pegomock.GetGenericMockFrom(verifier.mock).Verify(verifier.inOrderContext, verifier.invocationCountMatcher, "RunInitAndEnv", params)
-	return &Runner_RunInitAndEnv_OngoingVerification{mock: verifier.mock, methodInvocations: methodInvocations}
+	methodInvocations := pegomock.GetGenericMockFrom(verifier.mock).Verify(verifier.inOrderContext, verifier.invocationCountMatcher, "RunInitAndWorkspace", params)
+	return &Runner_RunInitAndWorkspace_OngoingVerification{mock: verifier.mock, methodInvocations: methodInvocations}
 }
 
-type Runner_RunInitAndEnv_OngoingVerification struct {
+type Runner_RunInitAndWorkspace_OngoingVerification struct {
 	mock              *MockRunner
 	methodInvocations []pegomock.MethodInvocation
 }
 
-func (c *Runner_RunInitAndEnv_OngoingVerification) GetCapturedArguments() (*logging.SimpleLogger, string, string, []string, *go_version.Version) {
+func (c *Runner_RunInitAndWorkspace_OngoingVerification) GetCapturedArguments() (*logging.SimpleLogger, string, string, []string, *go_version.Version) {
 	log, path, env, extraInitArgs, version := c.GetAllCapturedArguments()
 	return log[len(log)-1], path[len(path)-1], env[len(env)-1], extraInitArgs[len(extraInitArgs)-1], version[len(version)-1]
 }
 
-func (c *Runner_RunInitAndEnv_OngoingVerification) GetAllCapturedArguments() (_param0 []*logging.SimpleLogger, _param1 []string, _param2 []string, _param3 [][]string, _param4 []*go_version.Version) {
+func (c *Runner_RunInitAndWorkspace_OngoingVerification) GetAllCapturedArguments() (_param0 []*logging.SimpleLogger, _param1 []string, _param2 []string, _param3 [][]string, _param4 []*go_version.Version) {
 	params := pegomock.GetGenericMockFrom(c.mock).GetInvocationParams(c.methodInvocations)
 	if len(params) > 0 {
 		_param0 = make([]*logging.SimpleLogger, len(params[0]))
